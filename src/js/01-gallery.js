b25bcf7f5b688cf7b642f5c431d6ba405b0caadd
Jan Świątek <swiatek-jan@wp.pl>
2023-11-27 18:43:08 +0100

    T1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
-import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import { galleryItems } from './gallery-items.js';
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    (image) =>
+      `<li>
+      <a class="gallery__item" href="${image.original}">
+        <img
+          class="gallery__image"
+          src="${image.preview}"
+          alt="${image.description}"
+        />
+      </a>
+    </li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", markup);
 console.log(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', 
+    {
+    captionsData: "alt",
+    fadeSpeed: 500,
+    scrollZoom: true,
+    maxZoom: 2,
+    captionDelay: 250,
+});
+console.log(galleryItems);
+
+
